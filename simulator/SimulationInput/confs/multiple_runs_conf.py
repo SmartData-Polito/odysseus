598778fa4bd965a1e4caa643e8f9db1dfd7b41a3
AleCioc <alessandro@ciociola.me>
2020-03-18 18:07:54 +0100

    fix multiple runs config

--- simulator/SimulationInput/confs/multiple_runs_conf.py
+++ simulator/SimulationInput/confs/multiple_runs_conf.py
@@ -1,30 +1,32 @@
+import datetime
 import numpy as np
 
 sim_scenario_conf_grid = {
 
     "requests_rate_factor": [1],
     "n_cars_factor": [1],
+
     "time_estimation": [True],
     "queuing": [True],
-
-    "alpha": np.arange(20, 80, 20),
+    "alpha": np.arange(20, 80, 3),
     "beta": [80, 90, 100],
 
     "hub": [True],
     "hub_zone_policy": ["num_parkings"],
     "n_poles_n_cars_factor": [0.1],
 
-    "distributed_cps": [True],
-    "cps_placement_policy": ["num_parkings"],
-    "cps_zones_percentage": [0.1],
-
-    "user_contribution": [True],
-    "system_cps": [True],
-    "willingness": [0.99],
-
-    "battery_swap": [True],
-
     "relocation": [False],
     "finite_workers": [False],
+
+    "distributed_cps": [False],
+    "cps_placement_policy": ["num_parkings"],
+
+    "cps_zones_percentage": [0.1],
+
+    "user_contribution": [False],
+    "system_cps": [False],
+    "willingness": [0],
+
+    "battery_swap": [True],
 
 }
