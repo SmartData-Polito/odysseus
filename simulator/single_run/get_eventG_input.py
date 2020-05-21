b6b3783b35091a36dfc5482bcbde7aa6459571c1
AleCioc <alessandro@ciociola.me>
2020-05-21 10:50:27 +0200

    fix init vehicles in get traceB input

--- simulator/single_run/get_eventG_input.py
+++ simulator/single_run/get_eventG_input.py
@@ -1,20 +1,11 @@
-import datetime
-
 from simulator.simulation_input.sim_input import SimInput
 
 
 def get_eventG_input (conf_tuple):
 
-    # print ("Creating eventG simulation input ..")
-    # t0 = datetime.datetime.now()
-
     simInput = SimInput(conf_tuple)
-
     simInput.init_vehicles()
     simInput.init_hub()
     simInput.init_charging_poles()
-
-    # t1 = datetime.datetime.now()
-    # print (t1 - t0)
 
     return simInput
--- simulator/single_run/get_traceB_input.py
+++ simulator/single_run/get_traceB_input.py
@@ -1,23 +1,12 @@
-import datetime
-
 from simulator.simulation_input.sim_input import SimInput
 
 
 def get_traceB_input (conf_tuple):
 
-    # print ("Creating traceB simulation input ..")
-    # t0 = datetime.datetime.now()
-
-    simInput = SimInput\
-         (conf_tuple)
-
+    simInput = SimInput(conf_tuple)
     simInput.get_booking_requests_list()
     simInput.init_vehicles()
-    simInput.init_vehicles_dicts()
     simInput.init_hub()
     simInput.init_charging_poles()
-
-    # t1 = datetime.datetime.now()
-    # print (t1 - t0)
 
     return simInput
