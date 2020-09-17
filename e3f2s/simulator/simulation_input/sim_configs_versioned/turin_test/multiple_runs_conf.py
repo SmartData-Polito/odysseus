dd09b4b001710c3d227e8f61bc1ca4927b242dff
AleCioc <alessandro@ciociola.me>
2020-09-17 11:51:13 +0200

    add turin test config

---
+++ e3f2s/simulator/simulation_input/sim_configs_versioned/turin_test/multiple_runs_conf.py
@@ -0,0 +1,34 @@
+import datetime
+import numpy as np
+
+sim_scenario_conf_grid = {
+
+    "requests_rate_factor": np.arange(1, 6, 1),
+    "n_vehicles_factor": np.arange(1, 6, 1),
+
+    "time_estimation": [True],
+    "queuing": [True],
+
+    "alpha": [26],
+    "beta": [100],
+    "n_poles_n_vehicles_factor": np.arange(0.05, 0.45, 0.05),
+
+    "hub": [False],
+    "hub_zone_policy": [""],
+
+    "distributed_cps": [True],
+    "system_cps": [True],
+    "cps_placement_policy": ["num_parkings"],
+    "cps_zones_percentage": np.arange(0.1, 0.25, 0.05),
+
+    "battery_swap": [False],
+    "avg_reach_time": [20],
+    "avg_service_time": [0],
+
+    "n_workers": [1000],
+    "relocation": [False],
+
+    "user_contribution": [False],
+    "willingness": [0],
+
+}
---
+++ e3f2s/simulator/simulation_input/sim_configs_versioned/turin_test/sim_general_conf.py
@@ -0,0 +1,16 @@
+sim_general_conf_grid = {
+
+    # Fleet general parameters
+    "const_load_factor": [1],
+    "n_vehicles_original": [400],
+
+    # Space general parameters
+    "bin_side_length": [500],
+    "k_zones_factor": [1],
+
+    # Time general parameters
+    "year": [2017],
+    "month_start": [10],
+    "month_end": [11],
+
+}
