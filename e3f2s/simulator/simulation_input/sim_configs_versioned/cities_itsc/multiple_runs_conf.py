c90f90d59aa76d95f00c4a0ecd6e2539d4d10f42
AleCioc <alessandro@ciociola.me>
2020-10-22 15:29:45 +0200

    fix itsc conf

--- e3f2s/simulator/simulation_input/sim_configs_versioned/cities_itsc/multiple_runs_conf.py
+++ e3f2s/simulator/simulation_input/sim_configs_versioned/cities_itsc/multiple_runs_conf.py
@@ -9,9 +9,10 @@ sim_scenario_conf_grid = {
     "time_estimation": [True],
     "queuing": [True],
 
-    "alpha": [26],
+    "alpha_policy": ['auto'],
     "beta": [100],
-    "tot_n_charging_poles": np.arange(5, 150, 5),
+
+    "n_poles_n_vehicles_factor": np.arange(0.01, 0.2, 0.01),
 
     "hub": [False],
     "hub_zone_policy": [""],
