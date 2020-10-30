fc4d67a96f67a30bd332bdf554188c3fedcae16f
AleCioc <alessandro@ciociola.me>
2020-10-30 15:30:51 +0100

    remove factor from timeout

--- e3f2s/simulator/simulation/charging_strategies.py
+++ e3f2s/simulator/simulation/charging_strategies.py
@@ -107,7 +107,7 @@ class ChargingStrategy (ChargingPrimitives):
 					int(booking_request["destination_id"])
 				]
 
-			charging_station = self.charging_stations_dict[charging_zone_id].charging_station
+			charging_station = self.charging_stations_dict[zone].charging_station
 			resource = charging_station
 
 			if self.simInput.sim_scenario_conf["time_estimation"]:
