22c8a6e7884fc83b546cd8e5ac2d820c39ea5129
AleCioc <alessandro@ciociola.me>
2020-10-30 10:08:27 +0100

    add itsc set1c

--- e3f2s/simulator/simulation_input/sim_configs_versioned/isc2_set1c/sim_general_conf.py
+++ e3f2s/simulator/simulation_input/sim_configs_versioned/isc2_set1c/sim_general_conf.py
@@ -6,10 +6,10 @@ sim_general_conf_grid = {
 
     "data_source_id": ["big_data_db"],
     "sim_technique": ["eventG"],
-    "sim_scenario_name": ["isc2_set1b"],
+    "sim_scenario_name": ["isc2_set1c"],
 
     # Fleet general parameters
-    "const_load_factor": [1],
+    "const_load_factor": [False],
 
     # Space general parameters
     "bin_side_length": [500],
