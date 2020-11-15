8dcee926022317a12edb891ea9007c47b2a88371
AleCioc <alessandro@ciociola.me>
2020-11-15 11:05:12 +0100

    add fleet size configuration

---
+++ e3f2s/simulator/simulation_input/sim_configs_versioned/fleet_size/multiple_runs_conf.py
@@ -0,0 +1,34 @@
+import datetime
+import numpy as np
+
+sim_scenario_conf_grid = {
+
+    "requests_rate_factor": [1],
+    "n_vehicles": np.arange(100, 1000, 0.1),
+
+    "time_estimation": [True],
+    "queuing": [True],
+
+    "alpha_policy": ['auto'],
+    "beta": [100],
+    "tot_n_charging_poles": [200],
+
+    "hub": [False],
+    "hub_zone_policy": [""],
+
+    "distributed_cps": [True],
+    "system_cps": [True],
+    "cps_placement_policy": ["num_parkings"],
+    "cps_zones_percentage": [0.2],
+
+    "battery_swap": [False],
+    "avg_reach_time": [0],
+    "avg_service_time": [0],
+
+    "n_workers": [1000],
+    "relocation": [False],
+
+    "user_contribution": [False],
+    "willingness": [0],
+
+}
