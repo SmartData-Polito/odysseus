96ac5dd987d0732dc4e30a6b4b2cce1222a67ca1
AleCioc <alessandro@ciociola.me>
2020-11-17 15:49:47 +0100

    add new config to test charging relocation strategies

---
+++ e3f2s/simulator/simulation_input/sim_configs_versioned/charging_relocation_strategies_test/sim_general_conf.py
@@ -0,0 +1,23 @@
+sim_general_conf_grid = {
+
+    # Run configuration
+    "city": ["Torino"],
+    "sim_run_mode": ["multiple_runs"],
+
+    "data_source_id": ["big_data_db"],
+    "sim_technique": ["eventG"],
+    "sim_scenario_name": ["charging_relocation_strategies_test"],
+
+    # Fleet general parameters
+    "const_load_factor": [1],
+
+    # Space general parameters
+    "bin_side_length": [500],
+    "k_zones_factor": [1],
+
+    # Time general parameters
+    "year": [2017],
+    "month_start": [10],
+    "month_end": [11],
+
+}
