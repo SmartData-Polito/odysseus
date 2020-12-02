3f3aba07f2ef88cc72997db7171ffa88ed566023
Leonardo Tolomei <tolomei.leonardo@gmail.com>
2020-12-02 11:00:46 +0100

    add scooter relocation multiple runs configuration

---
+++ e3f2s/simulator/simulation_input/sim_configs_versioned/city_multiple_runs_scooter_relocation/sim_general_conf.py
@@ -0,0 +1,23 @@
+sim_general_conf_grid = {
+
+    # Run configuration
+    "city": ["Austin"],
+    "sim_run_mode": ["multiple_runs"],
+
+    "data_source_id": ["city_of_austin"],
+    "sim_technique": ["eventG"],
+    "sim_scenario_name": ["city_multiple_runs_scooter_relocation_magic_relocation"],
+
+    # Fleet general parameters
+    "const_load_factor": [1],
+
+    # Space general parameters
+    "bin_side_length": [200],
+    "k_zones_factor": [1],
+
+    # Time general parameters
+    "year": [2019],
+    "month_start": [7],
+    "month_end": [8],
+
+}
