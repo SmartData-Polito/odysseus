935a04c7ae5052e4cef9a4bdf826c6af16ec643e
AleCioc <alessandro@ciociola.me>
2020-12-20 11:51:10 +0100

    fully integrate citi bike NYC

--- e3f2s/city_data_manager/city_geo_trips/nyc_citi_bike_geo_trips.py
+++ e3f2s/city_data_manager/city_geo_trips/nyc_citi_bike_geo_trips.py
@@ -21,3 +21,29 @@ class NewYorkCityBikeGeoTrips(CityGeoTrips):
 
 		self.trips_ds_dict[self.trips_data_source_id].load_raw(self.year, self.month)
 		self.trips_ds_dict[self.trips_data_source_id].normalise(self.year, self.month)
+
+		self.trips_df_norm = self.trips_ds_dict[self.trips_data_source_id].load_norm(
+			self.year, self.month
+		)
+		self.trips = self.trips_df_norm.copy()
+		self.trips["geometry"] = self.trips_df_norm.apply(
+			lambda row: shapely.geometry.LineString([
+				shapely.geometry.Point(row["start_longitude"], row["start_latitude"]),
+				shapely.geometry.Point(row["end_longitude"], row["end_latitude"]),
+			]), axis=1
+		)
+		self.trips = gpd.GeoDataFrame(self.trips)
+		self.trips.crs = "epsg:4326"
+
+		self.trips_origins = self.trips_df_norm.copy()
+		self.trips_destinations = self.trips_df_norm.copy()
+		self.trips_origins["geometry"] = self.trips_origins.apply(
+			lambda row: shapely.geometry.Point(row["start_longitude"], row["start_latitude"]), axis=1
+		)
+		self.trips_destinations["geometry"] = self.trips_destinations.apply(
+			lambda row: shapely.geometry.Point(row["end_longitude"], row["end_latitude"]), axis=1
+		)
+		self.trips_origins = gpd.GeoDataFrame(self.trips_origins)
+		self.trips_origins.crs = "epsg:4326"
+		self.trips_destinations = gpd.GeoDataFrame(self.trips_destinations)
+		self.trips_destinations.crs = "epsg:4326"
