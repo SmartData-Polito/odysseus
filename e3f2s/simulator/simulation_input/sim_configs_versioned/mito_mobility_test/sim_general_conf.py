066b4808c29a96debeeeda0e96237413b5175880
AleCioc <alessandro@ciociola.me>
2020-12-21 02:02:45 +0100

    new config for mito-mobility test

---
+++ e3f2s/simulator/simulation_input/sim_configs_versioned/mito_mobility_test/sim_general_conf.py
@@ -0,0 +1,23 @@
+sim_general_conf_grid = {
+
+    # Run configuration
+    "city": ["New_York_City"],
+    "sim_run_mode": ["single_run"],
+
+    "data_source_id": ["citi_bike"],
+    "sim_technique": ["traceB"],
+    "sim_scenario_name": ["city_single_run_test"],
+
+    # Fleet general parameters
+    "const_load_factor": [1],
+
+    # Space general parameters
+    "bin_side_length": [500],
+    "k_zones_factor": [1],
+
+    # Time general parameters
+    "year": [2017],
+    "month_start": [1],
+    "month_end": [2],
+
+}
