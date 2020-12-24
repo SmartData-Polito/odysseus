1d46db545ea96ab77697d28827e00f6ec1180fd5
Leonardo Tolomei <tolomei.leonardo@gmail.com>
2020-12-24 20:26:21 +0100

    restore city_data_manager with scooter cities

--- e3f2s/city_data_manager/__main__.py
+++ e3f2s/city_data_manager/__main__.py
@@ -3,6 +3,14 @@ import datetime
 
 from e3f2s.city_data_manager.city_geo_trips.nyc_citi_bike_geo_trips import NewYorkCityBikeGeoTrips
 from e3f2s.city_data_manager.city_geo_trips.big_data_db_geo_trips import BigDataDBGeoTrips
+from e3f2s.city_data_manager.city_geo_trips.louisville_geo_trips import LouisvilleGeoTrips
+from e3f2s.city_data_manager.city_geo_trips.minneapolis_geo_trips import MinneapolisGeoTrips
+from e3f2s.city_data_manager.city_geo_trips.austin_geo_trips import AustinGeoTrips
+from e3f2s.city_data_manager.city_geo_trips.norfolk_geo_trips import NorfolkGeoTrips
+from e3f2s.city_data_manager.city_geo_trips.kansas_city_geo_trips import KansasCityGeoTrips
+from e3f2s.city_data_manager.city_geo_trips.chicago_geo_trips import ChicagoGeoTrips
+from e3f2s.city_data_manager.city_geo_trips.calgary_geo_trips import CalgaryGeoTrips
+
 
 parser = argparse.ArgumentParser()
 
@@ -39,11 +47,32 @@ for city in args.cities:
     for data_source_id in args.data_source_ids:
         for year in args.years:
             for month in args.months:
-
                 print(datetime.datetime.now(), city, data_source_id, year, month)
 
-                if data_source_id == "citi_bike":
+                if data_source_id == "city_of_louisville":
+                    geo_trips_ds = LouisvilleGeoTrips(year=int(year), month=int(month))
+
+                elif data_source_id == "city_of_minneapolis":
+                    geo_trips_ds = MinneapolisGeoTrips(year=int(year), month=int(month))
+
+                elif data_source_id == "city_of_austin":
+                    geo_trips_ds = AustinGeoTrips(year=int(year), month=int(month))
+
+                elif data_source_id == "city_of_norfolk":
+                    geo_trips_ds = NorfolkGeoTrips(year=int(year), month=int(month))
+
+                elif data_source_id == "city_of_kansas_city":
+                    geo_trips_ds = KansasCityGeoTrips(year=int(year), month=int(month))
+
+                elif data_source_id == "city_of_chicago":
+                    geo_trips_ds = ChicagoGeoTrips(year=int(year), month=int(month))
+
+                elif data_source_id == "city_of_calgary":
+                    geo_trips_ds = CalgaryGeoTrips(year=int(year), month=int(month))
+
+                elif data_source_id == "citi_bike":
                     geo_trips_ds = NewYorkCityBikeGeoTrips(year=int(year), month=int(month))
+
                 elif data_source_id == "big_data_db":
                     geo_trips_ds = BigDataDBGeoTrips(city, data_source_id, year=int(year), month=int(month))
 
