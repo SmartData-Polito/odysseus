4f438da9d8993bb8eb65da409977461f05c0ebd1
Matteo Mastrota <matteomastrota97@gmail.com>
2021-02-28 10:48:33 +0100

    test

--- e3f2s/webapp/__main__.py
+++ e3f2s/webapp/__main__.py
@@ -1,10 +1,14 @@
-from apis import create_app
+from e3f2s.webapp.apis import create_app
 #from e3f2s.webapp.emulate_module.simulator import Simulator
-#from e3f2s.webapp.emulate_module.city_data_manager import CityDataManager
-#from e3f2s.webapp.emulate_module.demand_modelling import DemandModelling
+from e3f2s.webapp.emulate_module.city_data_manager import CityDataManager
+from e3f2s.webapp.emulate_module.demand_modelling import DemandModelling
 
 app = create_app()
 
 if __name__ == '__main__':  # conditional only true if we run the script directly
-    app.run(debug=True)
+    #app.run(debug=True)
+    x = CityDataManager()
+    x.run()
+    a = DemandModelling()
+    a.run()
 
--- e3f2s/webapp/apis/__init__.py
+++ e3f2s/webapp/apis/__init__.py
@@ -5,7 +5,7 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)  # as an instance
 
-    from apis.api_cityDataManager.routes import api_cdm
+    from e3f2s.webapp.apis.api_cityDataManager.routes import api_cdm
     app.register_blueprint(api_cdm, url_prefix='/api_cdm')
 
     return app
